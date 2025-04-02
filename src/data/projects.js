02b0f9e6670a5eb7cc63a24737a6910cef46032f
Kz <aikikizuki@gmail.com>
2025-04-02 11:39:18 +0900

    READMEにARに関する情報を追加し、ARページのスクリプトを整理。動画再生機能を改善し、プロジェクトデータをクライアントサイドで管理する機能を追加。スタイルを調整し、ページのレイアウトを改善。

---
+++ src/data/projects.js
@@ -0,0 +1,21 @@
+// 全てのプロジェクト情報を一元管理
+export const projects = [
+    {
+        name: "松井ひなこ",
+        romaji: "MATSUI HINAKO",
+        keyword: "パフェに甘えて",
+        dir: "pafe"  // ディレクトリ名を追加
+    },
+    {
+        name: "林美羽",
+        romaji: "HAYASHI MIU",
+        keyword: "わかめプロジェクト",
+        dir: "wakame"
+    },
+    {
+        name: "花火",
+        romaji: "HANABI",
+        keyword: "FRCロボコン",
+        dir: "hanabi"
+    }
+]; 
