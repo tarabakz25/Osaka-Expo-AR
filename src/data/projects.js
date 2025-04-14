f1b8c5df10b45db38eca899f6b09d6bb23c82c0b
Kz <aikikizuki@gmail.com>
2025-04-14 11:50:29 +0900

    ARページのマーカー案内を改善し、プロジェクト情報を追加しました。また、不要な動画ファイルを削除し、ARLayoutのメタタグを更新しました。さらに、インデックスページのスタイルを調整し、波紋エフェクトを追加しました。

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,18 +4,79 @@ export const projects = [
         name: "松井ひなこ",
         romaji: "MATSUI HINAKO",
         keyword: "パフェに甘えて",
-        dir: "pafe"  // ディレクトリ名を追加
+        dir: "pafe" 
     },
     {
-        name: "林美羽",
-        romaji: "HAYASHI MIU",
-        keyword: "わかめプロジェクト",
-        dir: "wakame"
-    },
-    {
-        name: "花火",
-        romaji: "HANABI",
+        name: "Hanabi",
+        romaji: "",
         keyword: "FRCロボコン",
         dir: "hanabi"
-    }
+    },
+    {
+        name: "薦田葵",
+        romaji: "KOMODA MAMORU",
+        keyword: "わたあめ屋 comora",
+        dir: "mamoru"
+    },
+    {
+        name: "酒井盛光",
+        romaji: "SAKAI MORIMITSU",
+        keyword: "モノづくり、未来を変える",
+        dir: "morimitsu"
+    },
+    {
+        name: "奥村響ノ介",
+        romaji: "OKUMURA KYONOSUKE",
+        keyword: "ITブートキャンプ",
+        dir: "kyonosuke"
+    },
+    {
+        name: "逢沢紀乃",
+        romaji: "AIZAWA KINO",
+        keyword: "ワクワク、入学ほやほや",
+        dir: "kino"
+    },
+    {
+        name: "福西かい里",
+        romaji: "FUKUNISHI KAIRI",
+        keyword: "Merry Christmas！パーティー！",
+        dir: "kairi"
+    },
+    {
+        name: "若宮小芭音",
+        romaji: "WAKAMIYA KOHANE",
+        keyword: "ロボット開発",
+        dir: "kohane"
+    },
+    {
+        name: "永楽蒼弥",
+        romaji: "ERAKU SOYA",
+        keyword: "万博展示マネキン制作者",
+        dir: "soya"
+    },
+    {
+        name: "新井智己",
+        romaji: "ARAI TOMOKI",
+        keyword: "アプリ開発、生活を快適に",
+        dir: "tomoki"
+    },
+    {
+        name: "北尾光",
+        romaji: "KITAO HIKARI",
+        keyword: "Podcast部",
+        dir: "hikari"
+    },
+    {
+        name: "鈴木厚行",
+        romaji: "SUZUKI ATSUYUKI",
+        keyword: "スタッフ、楽しい授業づくり",
+        dir: "atsuyuki"
+    },
+    {
+        name: "上田冬真",
+        romaji: "UEDA TOMA",
+        keyword: "冬の神山",
+        dir: "toma"
+    },
+
 ]; 
